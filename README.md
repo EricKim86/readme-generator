65a30f6cee33c4988f55bdeede04fe69ea6d95b0
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-12-02 17:13:30 -0500

    added questions and general layout of the README.  README will not generate based on user input

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# readme-generator
+
+
+  # Eric's Awesome App
+ 
+  ![License](https://img.shields.io/badge/License-Apache-blue)
+  
+  ## Description
+  This app will make you 82% more cool in 42 minutes or less.
+
+
+  ## Table of Contents
+    
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Credit](#credit)
+  - [Questions](#questions)
+  - [Tests](#tests)
+
+
+  ## Installation
+  Just type stuff into the thingy
+
+
+  ## Usage
+  To be cool.
+
+
+  ## License
+  Apache
+ 
+
+  ## Credit
+  I did.
+
+
+  ## Questions / How to Contribute
+  Please visit me at: https://github.com/EricKim86. 
+
+  If you have any questions, feel free to reach me at: erickimek86@gmail.com.
+
+
+  ## Tests
+  I'm not sure.
--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of the README?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Write a short description of your project?',
+            name: 'description',
+        },
+        {
+            type: 'list',
+            message: 'What kind of license should your project have?',
+            name: 'license',
+            choices: ['MIT', 'GPLv2', 'Apache', 'Other']
+        },
+        {
+            type: 'input',
+            message: 'What command should be run to install dependencies?',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'What is the intended usage of the application?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Who contributed to the application?',
+            name: 'contributor',
+        },
+        {
+            type: 'input',
+            message: 'How can a user test the application?',
+            name: 'test',
+        },
+    ])
+    .then((response) => {
+        let readmeText = generateMarkdown(response);
+        fs.writeFile('README.md', readmeText, (err) =>
+            err ? console.error(err) : console.log('Success!')
+        );
+    })
